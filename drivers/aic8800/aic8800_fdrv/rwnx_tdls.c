f9e4adc712c3555d4498abf79e9d111ece07e838
xeu <sseu-buhzzi-github@mail.buhzzi.com>
2026-07-16 11:32:45 +0800

    fix: remove the union tag `u` and the removed field `action_code`

--- drivers/aic8800/aic8800_fdrv/rwnx_tdls.c
+++ drivers/aic8800/aic8800_fdrv/rwnx_tdls.c
@@ -117,11 +117,10 @@ rwnx_prep_tdls_direct(struct rwnx_hw *rwnx_hw, struct rwnx_vif *rwnx_vif,
 
     switch (action_code) {
     case WLAN_PUB_ACTION_TDLS_DISCOVER_RES:
-        skb_put(skb, 1 + sizeof(mgmt->u.action.u.tdls_discover_resp));
+        skb_put(skb, 1 + sizeof(mgmt->u.action.tdls_discover_resp));
         mgmt->u.action.category = WLAN_CATEGORY_PUBLIC;
-        mgmt->u.action.u.tdls_discover_resp.action_code = WLAN_PUB_ACTION_TDLS_DISCOVER_RES;
-        mgmt->u.action.u.tdls_discover_resp.dialog_token = dialog_token;
-        mgmt->u.action.u.tdls_discover_resp.capability =
+        mgmt->u.action.tdls_discover_resp.dialog_token = dialog_token;
+        mgmt->u.action.tdls_discover_resp.capability =
             cpu_to_le16(rwnx_get_tdls_sta_capab(rwnx_vif, status_code));
         break;
     default:
